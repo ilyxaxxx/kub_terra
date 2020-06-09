37107130d6b83485cd46382aecd0b163800d6290
Ilyxa <ilyxa.xxx@gmail.com>
2020-06-09 09:52:33 +0400

    first commit2

---
+++ deploy_kubernetes_CentOS/configure_phase2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+yum upgrade -y
+yum install -y docker kubelet
+
+systemctl enable docker && systemctl start docker
+systemctl enable kubelet && systemctl start kubelet
+
+cat << EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+ 
+sysctl --system
